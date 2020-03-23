4ce3226b4e2f3dc3de6282165c72365a6207ebfa
Jatin Kathuria <jtn.kathuria@gmail.com>
2020-03-23 19:04:12 +0800

    Added node-sass

---
+++ build/precache-manifest.6d2dbb352612823d8b20d18f64cfffad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b854e9f87d8bd91d3494b5625ded0a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1998e4b90641fb2ca5fa",
+    "url": "/static/css/main.f250fd53.chunk.css"
+  },
+  {
+    "revision": "b5b1023ffe929f030843",
+    "url": "/static/js/2.cac047d8.chunk.js"
+  },
+  {
+    "revision": "1998e4b90641fb2ca5fa",
+    "url": "/static/js/main.75b757dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
